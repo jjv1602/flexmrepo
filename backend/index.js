2a4d292816dfd48ec910def51c6788d7dcf9ea07
Joel <joel.varghese2020@vitbhopal.ac.in>
2023-12-19 17:32:35 +0530

    Configuration for deployment

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,21 @@ app.use('/api/trainer',trainerRoutes)
 app.use('/api/batch',batchRoutes)
 app.use('/api/booking',bookingRoutes)
 
+// ---------production ------------------
+if (process.env.NODE_ENV === 'production') {
+    const __dirname1 = path.resolve();
+    
+    app.use(express.static(path.join(__dirname1, "frontend/dist")));
+  
+    app.get("*", (req, res) =>{
+      res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
 
+    });
+  } else {
+    app.get("/", (req, res) => {
+      res.send("API is running.");
+    });
+  }
+// --------------
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,console.log(`Server started on port ${PORT} `));
